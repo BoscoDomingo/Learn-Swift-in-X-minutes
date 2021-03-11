c9946b709976ab4ff7b8f10c26bb8fdfa7effcec
Bosco Domingo <boscodomingob@gmail.com>
2021-03-11 10:28:16 +0100

    Result

--- learnswift.swift
+++ learnswift.swift
@@ -1136,3 +1136,83 @@ var aClass = SomeClass(1)
 var anotherClass = aClass
 anotherClass.changeSomeVariable(2)
 print(aClass.someVariable, anotherClass.someVariable) //2,2 since the original object was modified
+
+
+//MARK:- Result
+//Source: https://www.hackingwithswift.com/articles/161/how-to-use-result-in-swift
+enum NetworkError: Error {
+    case badURL
+}
+
+func fetchUnreadCount(from urlString: String, completionHandler: @escaping (Result<Int, NetworkError>) -> Void) {
+    guard let url = URL(string: urlString) else {
+        completionHandler(.failure(.badURL))
+        return
+    }
+
+    // complicated networking code here
+    print("Fetching \(url.absoluteString)...")
+    completionHandler(.success(5))
+}
+
+fetchUnreadCount(from: "https://www.hackingwithswift.com") { result in
+    switch result {
+    case .success(let count):
+        print("\(count) unread messages.")
+    case .failure(let error):
+        print(error.localizedDescription)
+    }
+}
+
+//Result has a get() method that either returns the successful value if it exists, or throws its error otherwise.
+//This allows you to convert Result into a regular throwing call, like this:
+fetchUnreadCount1(from: "https://www.hackingwithswift.com") { result in
+    if let count = try? result.get() {
+        print("\(count) unread messages.")
+    }
+}
+
+//Result has four other methods that may prove useful: map(), flatMap(), mapError(), and flatMapError().
+//Each of these give you the ability to transform either the success or error somehow, and the first two work similarly to the methods of the same name on Optional.
+
+//The map() method looks inside the Result, and transforms the success value into a different kind of value using a closure you specify.
+//However, if it finds failure instead, it just uses that directly and ignores your transformation.
+
+enum FactorError: Error {
+    case belowMinimum
+    case isPrime
+}
+
+func generateRandomNumber(maximum: Int) -> Result<Int, FactorError> {
+    if maximum < 0 {
+        // creating a range below 0 will crash, so refuse
+        return .failure(.belowMinimum)
+    } else {
+        let number = Int.random(in: 0...maximum)
+        return .success(number)
+    }
+}
+/*
+ When that’s called, the `Result` we get back will either be an integer or an error, so we could use `map()` to transform it:
+*/
+ let result1 = generateRandomNumber(maximum: 11)
+ let stringNumber = result1.map { "The random number is: \($0)." }
+
+/*
+As we’ve passed in a valid maximum number, result will be a success with a random number. So, using map() will take that random number, use it with our string interpolation, then return another Result type, this time of the type Result<String, FactorError>.
+
+However, if we had used generateRandomNumber(maximum: -11) then result would be set to the failure case with FactorError.belowMinimum. So, using map() would still return a Result<String, FactorError>, but it would have the same failure case and same FactorError.belowMinimum error.
+*/
+
+//If your transform closure returns a Result, flatMap() will return the new Result directly rather than wrapping it in another Result:
+func calculateFactors(for number: Int) -> Result<Int, FactorError> {
+    let factors = (1...number).filter { number % $0 == 0 }
+
+    if factors.count == 2 {
+        return .failure(.isPrime)
+    } else {
+        return .success(factors.count)
+    }
+}
+let result2 = generateRandomNumber(maximum: 10)
+let flatMapResult = result2.flatMap { calculateFactors(for: $0) }
